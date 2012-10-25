9c55aa64e736f2b386cc8b348335c34ad3478a5d
James Paterson <j@presstube.com>
2012-10-25 15:31:14 -0400

    init commit

--- src/globo.js
+++ src/globo.js
@@ -14,8 +14,8 @@ function init() {
 	c.Ticker.setFPS(30);
 	c.Ticker.addListener(stage);
 
-	globo.x = 240;
-	globo.y = 300;
+	globo.x = 320;
+	globo.y = 350;
 	stage.addChild(globo);
 
 	console.log(globo);
@@ -34,13 +34,13 @@ p.initialize = function() {
 		sections = [
 			new c.BitmapAnimation(
 				new c.SpriteSheet({"images": ["img/globo-1.png"], "frames": {"regY": 152, "height": 156, "regX": 57, "width": 106, "count": 8}, "animations": {"all": [0, 7]}})
-			),		
+			),
 			new c.BitmapAnimation(
 				new c.SpriteSheet({"images": ["img/globo-2.png"], "frames": {"regY": 103, "height": 184, "regX": 136, "width": 271, "count": 16}, "animations": {"all": [0, 15]}})
 			),
 			new c.BitmapAnimation(
 				new c.SpriteSheet({"images": ["img/globo-3.png"], "frames": {"regY": 95, "height": 117, "regX": 90, "width": 177, "count": 32}, "animations": {"all": [0, 31]}})
-			),		
+			),
 			new c.BitmapAnimation(
 				new c.SpriteSheet({"animations": {"all": [0, 16]}, "images": ["img/globo-4.png"], "frames": {"regY": 112, "height": 215, "regX": 106, "width": 210, "count": 17}})
 			)
@@ -60,7 +60,7 @@ p.initialize = function() {
 		};
 	});
 
-	_.each( _.toArray(sections).reverse(), function(section) { 
+	_.each( _.toArray(sections).reverse(), function(section) {
 		globo.addChild(section);
 	} );
 
