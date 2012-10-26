8feb447e21497c8dcec117faddb6a66ed4c4e373
James Paterson <j@presstube.com>
2012-10-26 17:05:14 -0400

    sharing spritesheet

--- src/globo.js
+++ src/globo.js
@@ -8,17 +8,17 @@ var c = createjs,
 function init() {
 
 	var canvas = document.getElementById("canvas"),
-		stage = new c.Stage(canvas),
-		globo = new pitarupaterson.Globo();
+		stage = new c.Stage(canvas);
 
 	c.Ticker.setFPS(30);
 	c.Ticker.addListener(stage);
 
-	globo.x = 320;
-	globo.y = 350;
-	stage.addChild(globo);
-
-	console.log(globo);
+	_.times(20, function() {
+		var globo = new pitarupaterson.Globo();
+		globo.x = Math.random()*600;
+		globo.y = Math.random()*900;
+		stage.addChild(globo);
+	});
 
 }
 
@@ -26,27 +26,24 @@ function init() {
 pitarupaterson.Globo = function() { this.initialize(); };
 p = pitarupaterson.Globo.prototype = new c.Container();
 p.Container_initialize = p.initialize;
+pitarupaterson.Globo.sectionSpriteSheets = [
+	new c.SpriteSheet({"images": ["img/globo-1.png"], "frames": {"regY": 152, "height": 156, "regX": 57, "width": 106, "count": 8}, "animations": {"all": [0, 7]}}),
+	new c.SpriteSheet({"images": ["img/globo-2.png"], "frames": {"regY": 103, "height": 184, "regX": 136, "width": 271, "count": 16}, "animations": {"all": [0, 15]}}),
+	new c.SpriteSheet({"images": ["img/globo-3.png"], "frames": {"regY": 95, "height": 117, "regX": 90, "width": 177, "count": 32}, "animations": {"all": [0, 31]}}),
+	new c.SpriteSheet({"animations": {"all": [0, 16]}, "images": ["img/globo-4.png"], "frames": {"regY": 112, "height": 215, "regX": 106, "width": 210, "count": 17}})
+];
 p.initialize = function() {
 
 	this.Container_initialize();
 
 	var globo = this,
 		sections = [
-			new c.BitmapAnimation(
-				new c.SpriteSheet({"images": ["img/globo-1.png"], "frames": {"regY": 152, "height": 156, "regX": 57, "width": 106, "count": 8}, "animations": {"all": [0, 7]}})
-			),
-			new c.BitmapAnimation(
-				new c.SpriteSheet({"images": ["img/globo-2.png"], "frames": {"regY": 103, "height": 184, "regX": 136, "width": 271, "count": 16}, "animations": {"all": [0, 15]}})
-			),
-			new c.BitmapAnimation(
-				new c.SpriteSheet({"images": ["img/globo-3.png"], "frames": {"regY": 95, "height": 117, "regX": 90, "width": 177, "count": 32}, "animations": {"all": [0, 31]}})
-			),
-			new c.BitmapAnimation(
-				new c.SpriteSheet({"animations": {"all": [0, 16]}, "images": ["img/globo-4.png"], "frames": {"regY": 112, "height": 215, "regX": 106, "width": 210, "count": 17}})
-			)
+			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[0] ),
+			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[1] ),
+			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[2] ),
+			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[3] )
 		],
 		yPositions = [0, 50, 116, 186];
-
 
 	_.each(sections, function(section, index) {
 		section.y = yPositions[index];
