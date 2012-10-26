be6a15a5b535b311e77c3b5bdda4fd2fc58f405d
James Paterson <j@presstube.com>
2012-10-26 19:07:13 -0400

    cleaning up code a bit

--- src/globo.js
+++ src/globo.js
@@ -1,7 +1,10 @@
 
 
+
+var pitarupaterson = {};
+
 var c = createjs,
-	pitarupaterson = {},
+	pp = pitarupaterson,
 	p; // shortcut for prototypes
 
 
@@ -14,52 +17,48 @@ function init() {
 	c.Ticker.addListener(stage);
 
 	_.times(20, function() {
-		var globo = new pitarupaterson.Globo();
+		var globo = new pp.Globo();
 		globo.x = Math.random()*600;
 		globo.y = Math.random()*900;
+		globo.rotation = Math.random()*360;
 		stage.addChild(globo);
 	});
 
 }
 
 
-pitarupaterson.Globo = function() { this.initialize(); };
-p = pitarupaterson.Globo.prototype = new c.Container();
+pp.Globo = function() { this.initialize(); };
+p = pp.Globo.prototype = new c.Container();
 p.Container_initialize = p.initialize;
-pitarupaterson.Globo.sectionSpriteSheets = [
+pp.Globo.sectionSpriteSheets = [
 	new c.SpriteSheet({"images": ["img/globo-1.png"], "frames": {"regY": 152, "height": 156, "regX": 57, "width": 106, "count": 8}, "animations": {"all": [0, 7]}}),
 	new c.SpriteSheet({"images": ["img/globo-2.png"], "frames": {"regY": 103, "height": 184, "regX": 136, "width": 271, "count": 16}, "animations": {"all": [0, 15]}}),
 	new c.SpriteSheet({"images": ["img/globo-3.png"], "frames": {"regY": 95, "height": 117, "regX": 90, "width": 177, "count": 32}, "animations": {"all": [0, 31]}}),
 	new c.SpriteSheet({"animations": {"all": [0, 16]}, "images": ["img/globo-4.png"], "frames": {"regY": 112, "height": 215, "regX": 106, "width": 210, "count": 17}})
 ];
+pp.Globo.sectionYPositions = [0, 50, 116, 186];
+
 p.initialize = function() {
 
 	this.Container_initialize();
 
 	var globo = this,
-		sections = [
-			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[0] ),
-			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[1] ),
-			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[2] ),
-			new c.BitmapAnimation( pitarupaterson.Globo.sectionSpriteSheets[3] )
-		],
-		yPositions = [0, 50, 116, 186];
+		sections = [];
 
-	_.each(sections, function(section, index) {
-		section.y = yPositions[index];
-		section.play();
-		section.onPress = function fargle(e) {
-			if (section.paused) {
-				section.play();
+	_.each(pp.Globo.sectionSpriteSheets, function(sectionSpriteSheets, index){
+		var bitmapAnimation = new c.BitmapAnimation(sectionSpriteSheets);
+		bitmapAnimation.y = pp.Globo.sectionYPositions[index];
+		bitmapAnimation.play();
+		bitmapAnimation.onPress = function () {
+			if (bitmapAnimation.paused) {
+				bitmapAnimation.play();
 			} else {
-				section.stop();
+				bitmapAnimation.stop();
 			}
 		};
+		sections.push(bitmapAnimation);
+		globo.addChildAt(bitmapAnimation, 0);
 	});
-
-	_.each( _.toArray(sections).reverse(), function(section) {
-		globo.addChild(section);
-	} );
 
 };
 
